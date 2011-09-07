0b74e3d5479963ccc7e5bab7106218366ab7dbce
maloep@googlemail.com <maloep@googlemail.com@1e8957de-daba-559d-2381-334ed5d54d8f>
2011-09-07 21:17:40 +0000

    - create userdata folder if not present - cleanup

--- trunk/default.py
+++ trunk/default.py
@@ -1,3 +1,15 @@
+# Copyright (C) 2011 Malte Loepmann (maloep@googlemail.com)
+#
+# This program is free software; you can redistribute it and/or modify it under the terms 
+# of the GNU General Public License as published by the Free Software Foundation; 
+# either version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program; 
+# if not, see <http://www.gnu.org/licenses/>.
 
 import xbmcplugin
 import xbmcgui
@@ -288,6 +300,13 @@ if(url == None):
 	url = BASE_URL
 
 path = xbmc.translatePath('special://profile/addon_data/%s' %(PLUGINID))
+
+if not os.path.exists(path):
+	try:
+		os.makedirs(path)
+	except:
+		path = ''
+
 success = login(os.path.join(path, 'cookies.txt'))
 
 if(success):
