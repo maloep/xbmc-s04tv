660e64776245f756b329824579fd155f25f743d3
maloep@googlemail.com <maloep@googlemail.com@1e8957de-daba-559d-2381-334ed5d54d8f>
2011-09-22 19:50:25 +0000

    - cleancleanup for repo submission

--- tags/0.4.1/default.py
+++ tags/0.4.1/default.py
@@ -28,11 +28,6 @@ import xbmcaddon
 __addon__ = xbmcaddon.Addon(id='plugin.video.s04tv')
 addonPath = __addon__.getAddonInfo('path')
 		
-BASE_RESOURCE_PATH = os.path.join(addonPath, "resources" )
-sys.path.append( os.path.join( BASE_RESOURCE_PATH, "lib" ) )
-sys.path.append( os.path.join( BASE_RESOURCE_PATH, "lib", "BeautifulSoup" ) )
-sys.path.append( os.path.join( BASE_RESOURCE_PATH, "lib", "mechanize" ) )
-
 from BeautifulSoup import BeautifulSoup
 import mechanize
 
